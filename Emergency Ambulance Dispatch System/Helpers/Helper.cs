7b6a07af7a1f0345403e3eb4269691aa89304440
khumaraoosman <khumarao@code.edu.az>
2026-04-29 19:57:33 +0400

    ambulans

---
+++ Emergency Ambulance Dispatch System/Helpers/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergency_Ambulance_Dispatch_System.Helpers
+{
+    internal class Helper
+    {
+    }
+}
---
+++ Emergency Ambulance Dispatch System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Emergency_Ambulance_Dispatch_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
