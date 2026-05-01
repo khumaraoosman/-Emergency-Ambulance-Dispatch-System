a2abaef23bc1874c035d803ee6177bb15d02fa03
asmarjb <asmarjb@code.edu.az>
2026-05-01 18:21:19 +0400

    service layer& console UI

--- Emergency Ambulance Dispatch System/Program.cs
+++ Emergency Ambulance Dispatch System/Program.cs
@@ -12,43 +12,48 @@ namespace Emergency_Ambulance_Dispatch_System
             string caseNo;
             Patient patient;
             Priority priority;
+            string phoneNumber;
+            string fullName;
+            string location;
+           
             EmergencyService service = new EmergencyService();
             do 
             {
                 Console.WriteLine("\r\n1. Create Emergency Case\r\n2. Assign Ambulance\r\n3. Start Dispatch\r\n4. Complete Case\r\n5. Get Case by CaseNo\r\n6. Get All Cases\r\n7. Filter by Status\r\n8. High Priority Cases\r\n9. Available Ambulances\r\n10. System Info\r\n0. Exit");
-                choose = Console.ReadLine();
+                choose = Console.ReadLine()!;
                 switch (choose) 
                 {
                     case "1":
                     fullName:
                         Console.WriteLine("pasiyentin adini daxil edin");
-                        string fullName = Console.ReadLine()!;
-                        if(Helpers.Helper.CheckFullName(fullName))
+                        fullName = Console.ReadLine()!;
+                        if(!Helpers.Helper.CheckFullName(fullName))
                         {
                             Console.WriteLine("duzgun ad daxil edin");
                             goto fullName;
                         }
                     phoneNumber:
                         Console.WriteLine("pasiyentin telefon nomresini daxil edin");
-                        string phoneNumber = Console.ReadLine()!;
-                        if(Helpers.Helper.CheckPhoneNumber(phoneNumber))
+                        phoneNumber = Console.ReadLine()!;
+                        if(!Helpers.Helper.CheckPhoneNumber(phoneNumber))
                         {
                             Console.WriteLine("telefon nomresini duzgun daxil edin");
                             goto phoneNumber;
                         }
                     location:
                         Console.WriteLine("pasiyentin adresini daxil edin");
-                        string location = Console.ReadLine()!;
-                        if(Helpers.Helper.CheckLocation(location))
+                        location = Console.ReadLine()!;
+                        if (!Helpers.Helper.CheckLocation(location))
                         {
                             Console.WriteLine("adres daxil edin");
                             goto location;
                         }
                         patient = new Patient(fullName, phoneNumber, location);
+                       
 
                     priotiret:
                         Console.WriteLine("prioritet daxil edin");
-                        if (System.Enum.TryParse(typeof(Priority), Console.ReadLine(), true, out object result))
+                        if (System.Enum.TryParse(typeof(Priority), Console.ReadLine(), true, out object? result))
                         {
                             priority = (Priority)result;
                         }
@@ -61,27 +66,63 @@ namespace Emergency_Ambulance_Dispatch_System
                         break;
 
                     case "2":
-                       Console.WriteLine("CaseNo daxil edin");
-                        caseNo = Console.ReadLine();
-                        service.AssignAmbulance(caseNo);
+                    caseNo:
+                        try
+                        {
+                            Console.WriteLine("CaseNo daxil edin");
+                            caseNo = Console.ReadLine()!;
+                            service.AssignAmbulance(caseNo);
+                        }
+                        catch (Exception)
+                        {
+
+                            goto caseNo;
+                        }
                         break;
 
                     case "3":
-                        Console.WriteLine("CaseNo daxil edin");
-                        caseNo = Console.ReadLine();
-                        service.StartDispatch(caseNo);
+                    caseNo1:
+                        try
+                        {
+                            Console.WriteLine("CaseNo daxil edin");
+                            caseNo = Console.ReadLine()!;
+                            service.StartDispatch(caseNo);
+
+                        }
+                        catch (Exception)
+                        {
+                            goto caseNo1;
+                        }
                         break;
 
                     case "4":
-                        Console.WriteLine("CaseNo daxil edin");
-                        caseNo = Console.ReadLine();
-                        service.CompleteCase(caseNo);
+                        caseNo2:
+                        try
+                        {
+                            Console.WriteLine("CaseNo daxil edin");
+                            caseNo = Console.ReadLine()!;
+                            service.CompleteCase(caseNo);
+                        }
+                        catch (Exception)
+                        {
+
+                            goto caseNo2;
+                        }
                         break;
 
                     case "5":
-                        Console.WriteLine("CaseNo daxil edin");
-                        caseNo= Console.ReadLine();
-                        service.GetCase(caseNo);
+                    caseNo3:
+                        try
+                        {
+                            Console.WriteLine("CaseNo daxil edin");
+                            caseNo = Console.ReadLine()!;
+                            service.GetCase(caseNo);
+                        }
+                        catch (Exception)
+                        {
+
+                            goto caseNo3;
+                        }
                         break;
 
                     case "6":
@@ -89,7 +130,18 @@ namespace Emergency_Ambulance_Dispatch_System
                         break;
 
                     case "7":
-                        Console.WriteLine("Case 7");
+                        status:
+                        Console.WriteLine("Status daxil edin (Created, Assigned, OnRoute, Completed)");
+                        if (System.Enum.TryParse(typeof(EmergencyStatus), Console.ReadLine(), true, out object? resultStatus))
+                        {
+                             EmergencyStatus resultstatus = (EmergencyStatus)resultStatus;
+                             service.GetCasesByStatus(resultstatus);
+                        }
+                        else
+                        {
+                            Console.WriteLine("duzgun status daxil edin");
+                            goto status;
+                        }
                         break;
 
                     case "8":
@@ -106,7 +158,9 @@ namespace Emergency_Ambulance_Dispatch_System
                      case "0":
                         IsExit = true;
                         break;
-
+                    default:
+                        Console.WriteLine("duzgun secim edin");
+                        break;
                 }
             } 
             while (!IsExit);
